7d1f8e7557132aa77985b40130e20240af03b188
MY RAILS <jhambrohan44@gmail.com>
2017-09-17 23:58:21 +0530

    added configuation of food api in model

---
+++ app/models/recipe.rb
@@ -0,0 +1,14 @@
+
+class Recipe < ApplicationRecord
+
+		include HTTParty
+
+		key_value = "ffb6357d087943994b16fa55a5e8e095"
+		base_uri "http://food2fork.com/api/search"
+		default_params key: key_value
+		format :json
+
+		def self.for keyword
+    		get("", query: {q: keyword})["recipes"]
+  		end
+end
